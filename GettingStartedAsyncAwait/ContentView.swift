810c83db21fe6a06498424683f16eb3ea77cb390
larissa.silva <larissa.silva@sejaefi.com.br>
2024-09-20 14:21:09 -0300

    feat: getting started with async and await

--- GettingStartedAsyncAwait/ContentView.swift
+++ GettingStartedAsyncAwait/ContentView.swift
@@ -1,20 +1,59 @@
-//
-//  ContentView.swift
-//  GettingStartedAsyncAwait
-//
-//  Created by Mohammad Azam on 7/9/21.
-//
-
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct CurrentDate: Decodable, Identifiable {
+
+    let id = UUID()
+    let date: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case date = "date"
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ContentView: View {
+
+    @State private var currentDates: [CurrentDate] = []
+
+    private func getDate() async throws -> CurrentDate? {
+        guard let url = URL(string: "https://ember-sparkly-rule.glitch.me/current-date") else {
+            fatalError()
+        }
+
+        let (data, _) = try await URLSession.shared.data(from: url)
+        return try? JSONDecoder().decode(CurrentDate.self, from: data)
+    }
+
+    private func populateDates() async {
+        do {
+            guard let date = try await getDate() else { return }
+            currentDates.append(date)
+        } catch {
+            print(error)
+        }
+    }
+
+    var body: some View {
+        NavigationView {
+            List(currentDates) { currentDate in
+                Text(currentDate.date)
+            }.listStyle(.plain)
+            .navigationTitle("Dates")
+            .navigationBarItems(trailing: Button(action: {
+                Task {
+                    await populateDates()
+                }
+            }, label: {
+                Image(systemName: "arrow.clockwise.circle")
+            }))
+            .task {
+                await populateDates()
+            }
+        }
+    }
+}
+
+struct ContentViewPreviews: PreviewProvider {
+    
     static var previews: some View {
         ContentView()
     }
--- GettingStartedAsyncAwait/GettingStartedAsyncAwaitApp.swift
+++ GettingStartedAsyncAwait/GettingStartedAsyncAwaitApp.swift
@@ -1,14 +1,8 @@
-//
-//  GettingStartedAsyncAwaitApp.swift
-//  GettingStartedAsyncAwait
-//
-//  Created by Mohammad Azam on 7/9/21.
-//
-
 import SwiftUI
 
 @main
 struct GettingStartedAsyncAwaitApp: App {
+
     var body: some Scene {
         WindowGroup {
             ContentView()
